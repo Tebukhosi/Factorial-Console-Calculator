027cfc1e472b7c42e37c51b9f702a15208873a15
Tebukhosi Mathebula <129428548+Tebukhosi@users.noreply.github.com>
2023-09-05 09:05:17 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number you'd like a factorial number of:");
-        int n = scanner.nextInt();  // Change n to calculate factorial of a different number
+        int n = scanner.nextInt();  
         long factorial = 1;
 
         for (int i = 1; i <= n; i++) {
